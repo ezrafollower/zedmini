f8a3a52f55b56ecc1ebf4cfed8668a4dcdb60189
ezrafollower <ezrafollower@gmail.com>
2022-05-06 03:01:20 -0500

    submod

---
+++ catkin_ws/build/zed-ros-wrapper/zed_nodelets/CMakeFiles/zed_nodelets_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zed_nodelets_gencfg"
+  "/home/argnctu/zedmini/catkin_ws/devel/include/zed_nodelets/ZedConfig.h"
+  "/home/argnctu/zedmini/catkin_ws/devel/share/zed_nodelets/docs/ZedConfig.dox"
+  "/home/argnctu/zedmini/catkin_ws/devel/share/zed_nodelets/docs/ZedConfig-usage.dox"
+  "/home/argnctu/zedmini/catkin_ws/devel/lib/python2.7/dist-packages/zed_nodelets/cfg/ZedConfig.py"
+  "/home/argnctu/zedmini/catkin_ws/devel/share/zed_nodelets/docs/ZedConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/zed_nodelets_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
